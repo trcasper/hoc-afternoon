3b7bda4cd5c85b74bddfe41640d6c1a0fdf25f2d
trcasper <trcasper90@gmail.com>
2019-10-22 14:33:56 -0600

    complete project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import CurrencyHOC from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
-    );
+      <>
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
+      </>
+    )
   }
 }
 
-export default App;
+export default App
